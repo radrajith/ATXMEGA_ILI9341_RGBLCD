1270e08add3e9afbe6437755763dddd9c67f8f36
Rajith Radhakrishnan <radrajith@gmail.com>
2016-12-03 19:50:01 -0500

    added startup screen

--- LCD_ILI9341_ESE_323/ili9341.h
+++ LCD_ILI9341_ESE_323/ili9341.h
@@ -181,7 +181,7 @@ void clear(uint16_t color);
 void drawPixel(uint16_t x1, uint16_t y1, uint16_t color);
 void drawVLine(uint16_t x, uint16_t y, uint16_t h, uint16_t color);
 void drawHLine(uint16_t x, uint16_t y, uint16_t w, uint16_t color);
-void fillrect(uint16_t color);
+void fillrect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color){
 void fillScreen();
 void setRotation(uint8_t x);
 
--- LCD_ILI9341_ESE_323/main.c
+++ LCD_ILI9341_ESE_323/main.c
@@ -10,12 +10,36 @@
 #include "gfx.h"
 #include "adc.h"
 #include "snake.h"
+# define F_CPU 1000000UL 
 void rotate(){
 	setRotation(2);
 }
 void readADC(){
 	adc_init(POS_ADC0, NEG_ADC1);		//the adc used for pos and neg are passed in
 	int current = adc_get();
+}
+void startupScreen(){
+	int color = 0;
+	int a = 0;
+	int axis = 0;
+	for(int i = 0; i<((TFTWIDTH-130)/2) ; i=i+5){
+		if((a%2) == 0){
+			color = ILI9341_WHITE;
+		}
+		else{
+			color = ILI9341_BLACK;
+		}
+		a++;
+		fillrect(i,i, (TFTWIDTH-(2*i)), (TFTHEIGHT-(2*i)), color);
+		axis = i;
+	}
+	gfx_setCursor(axis+10, axis+10);
+	gfx_setTextColor(ILI9341_RED);
+	gfx_setTextSize(3);
+	//setRotation(3);
+	gfx_print("ESE 323");
+
+	_delay_ms(15000);
 }
 
 int main(void)
@@ -51,13 +75,10 @@ int main(void)
 	//################Finish building gfx_println function###################
 	//gfx_write('R');
 	drawPixel(50,50, ILI9341_BLACK);
-	drawHLine(0, getHeight()/2, getWidth(), ILI9341_BLUE);
-	drawVLine(getWidth()/2, 0, getHeight(), ILI9341_BLUE);
-	for (int i = 1; i < 318; i++)
-	{
-		
-		drawPixel(i , sin(3.14) ,  ILI9341_CYAN);
-	}
+	startupScreen();
+	
+	int b = 90;
+	int a = 0;
 	while(1)
 	{
 		//snake_run();
@@ -65,7 +86,28 @@ int main(void)
 		//_delay_ms(100000);
 		//clear(ILI9341_GREEN);
 		//clear(ILI9341_BLACK);
+		/*
+		clear(ILI9341_BLACK);
+			drawHLine(0, getHeight()/2, getWidth(), ILI9341_BLUE);
+			drawVLine(getWidth()/2, 0, getHeight(), ILI9341_BLUE);
+		for (int i = 1; i < 318; i++)
+		{
+			int a;
+			a =  119 + (sin(((i*1.13)*3.14) / 180) * 95);
+			drawPixel(i , a,  ILI9341_CYAN);
+		}
+		clear(ILI9341_BLACK);
+			drawHLine(0, getHeight()/2, getWidth(), ILI9341_BLUE);
+			drawVLine(getWidth()/2, 0, getHeight(), ILI9341_BLUE);
 		
+		for (int i = 1; i < 318; i++)
+		{
+			int a;
+			a =  119 + (cos(((i*1.13)*3.14) / 180) * 95);
+			drawPixel(i , a,  ILI9341_CYAN);
+		}
+		
+		*/
 		//_delay_ms(1000);
 	}
 }
